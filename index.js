2ff7c60e938f76557b51be0e2761abfdb911e1fd
Kota Haruna <k.haruna.101@gmail.com>
2020-04-28 20:10:30 +0900

    Herokuで起動するための準備

---
+++ index.js
@@ -0,0 +1,59 @@
+'use strict';
+const http = require('http');
+const pug = require('pug');
+const server = http
+	.createServer((req, res) => {
+		const now = new Date();
+		console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+		res.writeHead(200, {
+			'Content-Type': 'text/html; charset=utf-8'
+		});
+		switch(req.method){
+			case 'GET':
+				if(req.url === '/enquetes/yaki-shabu'){
+					res.write(pug.renderFile('./form.pug', {
+				    		path: req.url,
+				    		firstItem: '焼肉',
+				    		secondItem: 'しゃぶしゃぶ'
+					}));
+				}
+				else if(req.url === '/enquetes/rice-bread'){
+					res.write(pug.renderFile('./form.pug', {
+				    		path: req.url,
+				    		firstItem: 'ご飯',
+				    		secondItem: 'パン'
+					}));
+				}
+				res.end();
+				break;
+			case 'POST':
+				let rawData = '';
+				req.on('data', (chunk) => {
+					rawData = rawData + chunk;
+				}).on('end', () => {
+				    const decoded = decodeURIComponent(rawData);
+				    const qs = require('querystring');
+				    const answer = qs.parse(decoded);
+				    console.info('[' + now + '] 投稿: ' + answer['favorite'] + ' (' + answer['name'] + ')');
+				    res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
+					 answer['name'] + 'さんは' + answer['favorite'] + 'を投稿しました</h1></body></html>');
+				    res.end();
+				});
+				break;
+			case 'DELETE':
+				res.write('DELETE ' + req.url);
+				break;
+			default:
+				break;
+		}
+	})
+	.on('error', e => {
+		console.error('[' + new Date() + '] Server Error', e);
+	})
+	.on('clientError', e => {
+		console.error('[' + new Date() + '] Client Error', e);
+	});
+const port = process.env.PORT || 8000;
+server.listen(port, () => {
+	console.info('[' + new Date() + '] Listening on ' + port);
+});
