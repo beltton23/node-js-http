f7b0d60c39253e58d0edb15f41c3a8c339ee40e8
Kota Haruna <k.haruna.101@gmail.com>
2020-04-29 14:26:14 +0900

    ログから日付を削除

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const http = require('http');
 const pug = require('pug');
 const server = http
 	.createServer((req, res) => {
-		const now = new Date();
-		console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
+		console.info('Requested by ' + req.connection.remoteAddress);
 		res.writeHead(200, {
 			'Content-Type': 'text/html; charset=utf-8'
 		});
@@ -34,7 +33,7 @@ const server = http
 				    const decoded = decodeURIComponent(rawData);
 				    const qs = require('querystring');
 				    const answer = qs.parse(decoded);
-				    console.info('[' + now + '] 投稿: ' + answer['favorite'] + ' (' + answer['name'] + ')');
+				    console.info('投稿: ' + answer['favorite'] + ' (' + answer['name'] + ')');
 				    res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
 					 answer['name'] + 'さんは' + answer['favorite'] + 'を投稿しました</h1></body></html>');
 				    res.end();
@@ -48,12 +47,12 @@ const server = http
 		}
 	})
 	.on('error', e => {
-		console.error('[' + new Date() + '] Server Error', e);
+		console.error('Server Error', e);
 	})
 	.on('clientError', e => {
-		console.error('[' + new Date() + '] Client Error', e);
+		console.error('Client Error', e);
 	});
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-	console.info('[' + new Date() + '] Listening on ' + port);
+	console.info('Listening on ' + port);
 });
